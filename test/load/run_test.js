c527aa18c492617f9a98aac2058601cec5f03f76
Mauro Centurion <mauro@neocoast.com>
2020-08-27 11:26:54 -0300

    Add load test with async endpoint

--- test/load/run_test.js
+++ test/load/run_test.js
@@ -20,10 +20,13 @@ const startServer = async () => {
 
 const runArtilleryTest = async (path) => {
   try {
+    process.stdout.write(`-  Test ${path}\r`);
     const command = './node_modules/artillery/bin/artillery run ' + path;
     execSync(command);
+    console.log(chalk.green('\u{2713}'), ' Test ' + path + ' ran successfully');
     return path;
   } catch (error) {
+    console.log(chalk.red('\u{2717}'), ' Test ' + path + ' failed');
     const wrappedError = new Error(error);
     wrappedError.file = path;
 
@@ -39,7 +42,7 @@ const runArtillery = async () => {
 
     const results = await Promise.allSettled(tests.map((testFile) => runArtilleryTest(loadPath + '/' + testFile)));
 
-    console.log(chalk.underline('Load test results:'));
+    console.log('\r\n', chalk.underline('Load test results:'));
 
     results.forEach(({ status, reason, value }) => {
       console.log(
@@ -57,25 +60,27 @@ const runArtillery = async () => {
     }
 
   } catch (error) {
-    console.log(chalk.red('\n\u{2717}'),' Error running artillery')
+    console.log(chalk.red('\r\n\u{2717}'),' Error running artillery')
     throw error;
   }
 }
 
 const runTest = async () => {
   try {
+    console.log('\r\n',chalk.blue('\u{25CC}'), ' Starting server...');
     const server = await startServer();
 
-    await sleep(2);
+    await sleep(1);
+    console.log(chalk.green('\u{2713}'), ' Server started\n');
 
     await runArtillery();
 
     server.kill();
 
-    console.log('\n', chalk.green('\u{2713}'), ' Tests ran successfully');
+    console.log('\r\n', chalk.green('\u{2713}'), ' Tests ran successfully');
     process.exit(0);
   } catch (error) {
-    console.log('\n', chalk.red('\u{2717}'), ' Error running tests');
+    console.log('\r\n', chalk.red('\u{2717}'), ' Error running tests');
     if (server && server.kill) {
       server.kill();
     }
